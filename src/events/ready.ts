6c965c79838fb4e7b314c852c78c59a7f38be722
Badosz <badoszk@gmail.com>
2020-02-04 18:42:50 +0100

    presence

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,4 +1,17 @@
+import { core } from "../index";
+
 export function run () : void
 {
-    console.log("Badosz Bot is ready.")
+    console.log("Badosz Bot is ready.");
+    (function setPresence() {
+        core.user.setPresence(
+          { game:
+            { type: 'WATCHING',
+               name: `${core.prefix}help`
+            },
+            status: 'online'
+          }
+        )
+        setTimeout(setPresence, 120000)
+      })()
 }
