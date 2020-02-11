7c58cd0c95731b8e60fc7c2b0bd1b165ec88e775
Badosz <badoszk@gmail.com>
2020-02-11 15:39:54 +0100

    user command

---
+++ src/plugins/info/commands/user.ts
@@ -0,0 +1,29 @@
+import { Image_command, Command_output } from "../../../structures/command";
+import { get_user } from "../../../utils/user";
+
+export = new Image_command ({
+    trigger : "user",
+    developer: false,
+    output : async ({message, args = []}: Command_output) => 
+    {
+        const user = await get_user(message, args)
+        const member = await message.guild.fetchMember(user)
+
+        return {
+            text: `\`Username:\` ${user.tag}\n` +
+                  `\`Nick:\` ${member.nickname ? member.nickname : user.username}\n` +
+                  `\`Bot:\` ${user.bot ? 'Yes' : 'No'}\n` +
+                  `\`ID:\` ${user.id}\n` +
+                  `\`Status:\` ${user.presence.status.replace(/online/g, `Online`)
+                                                     .replace(/idle/g, `Idle`)
+                                                     .replace(/dnd/g, `Do Not Disturb`)
+                                                     .replace(/offline/g, `Offline`)}\n` +
+                  `\`Muted:\` ${member.serverMute ? 'Yes' : 'No'}\n` +
+                  `\`Color:\` ${member.displayHexColor}\n` +
+                  `\`Highest Role:\` ${member.highestRole.name.replace(/@everyone/g, '-')}\n` +
+                  `\`Joined At:\` ${member.joinedAt.toUTCString()}\n` +
+                  `\`Created At:\` ${user.createdAt.toUTCString()}\n`,
+            thumbnail: user.displayAvatarURL
+        }
+    }
+})
