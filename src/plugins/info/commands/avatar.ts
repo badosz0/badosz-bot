aba09647ede7b4d61ff6124b6c28f41109c0879e
Badosz <badoszk@gmail.com>
2020-02-11 12:51:01 +0100

    avatar command

---
+++ src/plugins/info/commands/avatar.ts
@@ -0,0 +1,20 @@
+import { Image_command, Command_output } from "../../../structures/command";
+import { core } from "../../../index";
+import { get_user } from "../../../utils/user";
+
+export = new Image_command ({
+    trigger : "avatar",
+    developer: false,
+    output : async ({message, args = []}: Command_output) => 
+    {
+        const user = await get_user(message, args)
+        const source = user.displayAvatarURL
+        const png = source.replace('.gif', '.png')
+        const webp = png.replace('.png', '.webp')
+        const jpg = png.replace('.png', '.jpg')
+        return {
+            text: `${user.tag} (${user.id})\n[source](${source}) | [png](${png}) | [webp](${webp}) | [jpg](${jpg})`,
+            image: source
+        }
+    }
+})
