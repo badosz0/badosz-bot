a86a82c66b293e65c678c34d8b19bd4b74150a7a
Badosz <badoszk@gmail.com>
2020-03-10 22:44:41 +0100

    better members

--- src/plugins/info/commands/members.ts
+++ src/plugins/info/commands/members.ts
@@ -2,10 +2,31 @@ import { Text_command, Command_output } from "../../../structures/command";
 import { core } from "../../../index";
 import { Message } from "discord.js";
 
+const programming = [
+    "Eclipse IDE", 
+    "WebStorm", 
+    "IntelliJ IDEA Ultimate", 
+    "PhpStorm", 
+    "Rider", 
+    "RubyMine", 
+    "Clion", 
+    "PyCharm", 
+    "IntelliJ IDEA", 
+    "JetBrains IDE", 
+    "GoLand", 
+    "Sublime Text", 
+    "PyCharm Professional", 
+    "Atom", 
+    "Atom Editor", 
+    "Visual Studio Code", 
+    "Visual Studio", 
+    "Android Studio"
+]
+
 function by_status(message: Message, status: string, total: number) : string
 {
     const count =  message.guild.members.filter(m => m.presence.status == status && !m.user.bot).size
-    return `\`${Math.round(count/total*100)}%\` [ ${count} ]`
+    return `\`${count}\` [ ${(count/total*100).toFixed(2)}% ]`
 }
 
 function by_game(message: Message, games: string[], total: number) : string
@@ -15,13 +36,13 @@ function by_game(message: Message, games: string[], total: number) : string
     {
         count += message.guild.members.filter(m => m.presence.game && m.presence.game.name == game && !m.user.bot).size
     })
-    return `\`${Math.round(count/total*100)}%\` [ ${count} ]`
+    return `\`${count}\` [ ${(count/total*100).toFixed(2)}% ]`
 }
 
 function nothing(message: Message, total: number) : string
 {
     const count = message.guild.members.filter(m => !m.presence.game && !m.user.bot).size
-    return `\`${Math.round(count/total*100)}%\` [ ${count} ]`
+    return `\`${count}\` [ ${(count/total*100).toFixed(2)}% ]`
 }
 
 export = new Text_command ({
@@ -36,7 +57,7 @@ export = new Text_command ({
         `<:dcDnd:495281269609857024> ${by_status(message, "dnd", total_members)} Dnd\n` +
         `<:dcOffline:495281269379432449> ${by_status(message, "offline", total_members)} Offline\n\n` +
 
-        `:keyboard: ${by_game(message, ["Eclipse IDE", "WebStorm", "IntelliJ IDEA Ultimate", "PhpStorm", "Rider", "RubyMine", "Clion", "PyCharm", "IntelliJ IDEA", "JetBrains IDE", "GoLand", "Sublime Text", "PyCharm Professional", "Atom", "Atom Editor", "Visual Studio Code", "Visual Studio", "Android Studio"], total_members)} Programming\n` +
+        `:keyboard: ${by_game(message, programming, total_members)} Programming\n` +
         `:musical_note: ${by_game(message, ["Spotify"], total_members)} Listening to Spotify\n` +
         `:sleeping_accommodation: ${nothing(message, total_members)} Doing Nothing`
 
