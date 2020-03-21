2c7b7a841e4d6e2f6c75c6c027d16890bb7aad76
Badosz <badoszk@gmail.com>
2020-03-21 19:44:46 +0100

    server rewrite

--- src/plugins/info/commands/server.ts
+++ src/plugins/info/commands/server.ts
@@ -22,20 +22,70 @@ export = new Command ({
             "us-south": ":flag_us: United States of America",
             "us-west":":flag_us: United States of America"
         }
+        const verification = [
+            "None", 
+            "Low",
+            "Medium", 
+            "(╯°□°）╯︵  ┻━┻", 
+            "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻"
+        ]
 
         let server = message.client.guilds.get(args[0])
         if (!server) server = message.client.guilds.find(guild => guild.name === args.join(' '))
         if (!server) server = message.guild
 
         return {
-            text: `**Name:** ${server.name}\n` +
-                  `**ID:** ${server.id}\n` +
-                  `**Owner:** ${server.owner.user.tag}\n` +
-                  `**Members:** ${server.members.size}\n` +
-                  `**Region:** ${regions[server.region]}\n` +
-                  `**Text Channels:** ${server.channels.filter(channel => channel.type == 'text').size}\n` +
-                  `**Voice Channels:** ${server.channels.filter(channel => channel.type == 'voice').size}\n` +
-                  `**Created At:** ${server.createdAt.toUTCString()}\n`,
+            fields: [
+                {
+                    title: "Name",
+                    text: server.name,
+                    inline: true
+                },
+                {
+                    title: "Owner",
+                    text: server.owner.user.tag,
+                    inline: true
+                },
+                {
+                    title: "ID",
+                    text: server.id,
+                    inline: true
+                },
+                {
+                    title: "Members",
+                    text: server.members.size,
+                    inline: true
+                },
+                {
+                    title: "Region",
+                    text: regions[server.region],
+                    inline: true
+                },
+                {
+                    title: "Verification Level",
+                    text: verification[server.verificationLevel],
+                    inline: true
+                },
+                {
+                    title: "Emojis",
+                    text: server.emojis.size,
+                    inline: true
+                },
+                {
+                    title: "Text Channels",
+                    text: server.channels.filter(channel => channel.type == 'text').size,
+                    inline: true
+                },
+                {
+                    title: "Voice Channels",
+                    text: server.channels.filter(channel => channel.type == 'voice').size,
+                    inline: true
+                },
+                {
+                    title: "Created At",
+                    text: server.createdAt.toUTCString()
+                },
+            ],
             thumbnail: server.icon ? server.iconURL : `https://dummyimage.com/128/7289DA/FFFFFF/&text=${encodeURIComponent(server.nameAcronym)}`
         }
     }
