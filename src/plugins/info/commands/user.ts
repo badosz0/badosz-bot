3c8ce903ddc1f951f0c9e41ea190fc8d77746ac5
Badosz <badoszk@gmail.com>
2020-03-21 19:51:30 +0100

    user rewrite

--- src/plugins/info/commands/user.ts
+++ src/plugins/info/commands/user.ts
@@ -11,19 +11,67 @@ export = new Command ({
         const member = await message.guild.fetchMember(user)
 
         return {
-            text: `**Username:** ${user.tag}\n` +
-                  `**Nick:** ${member.nickname ? member.nickname : user.username}\n` +
-                  `**Bot:** ${user.bot ? 'Yes' : 'No'}\n` +
-                  `**ID:** ${user.id}\n` +
-                  `**Status:** ${user.presence.status.replace(/online/g, `Online`)
-                                                     .replace(/idle/g, `Idle`)
-                                                     .replace(/dnd/g, `Do Not Disturb`)
-                                                     .replace(/offline/g, `Offline`)}\n` +
-                  `**Muted:** ${member.serverMute ? 'Yes' : 'No'}\n` +
-                  `**Color:** ${member.displayHexColor}\n` +
-                  `**Highest Role:** ${member.highestRole.name.replace(/@everyone/g, '-')}\n` +
-                  `**Joined At:** ${member.joinedAt.toUTCString()}\n` +
-                  `**Created At:** ${user.createdAt.toUTCString()}\n`,
+            fields: [
+                {
+                    title: "Username",
+                    text: user.tag,
+                    inline: true
+                },
+                {
+                    title: "Nick",
+                    text: member.nickname ? member.nickname : user.username,
+                    inline: true
+                },
+                {
+                    title: "ID",
+                    text: user.id,
+                    inline: true
+                },
+                {
+                    title: "Bot",
+                    text: user.bot ? 'Yes' : 'No',
+                    inline: true
+                },
+
+                {
+                    title: "Status",
+                    text: user.presence.status.replace(/online/g, `Online`)
+                                              .replace(/idle/g, `Idle`)
+                                              .replace(/dnd/g, `Do Not Disturb`)
+                                              .replace(/offline/g, `Offline`),
+                    inline: true
+                },
+                {
+                    title: "Muted",
+                    text: member.serverMute ? 'Yes' : 'No',
+                    inline: true
+                },
+                {
+                    title: "Deafed",
+                    text: member.serverDeaf ? 'Yes' : 'No',
+                    inline: true
+                },
+
+                {
+                    title: "Highest Role",
+                    text: member.highestRole.name.replace(/@everyone/g, '-'),
+                    inline: true
+                },
+                {
+                    title: "Color",
+                    text: member.displayHexColor,
+                    inline: true
+                },
+
+                {
+                    title: "Created At",
+                    text: user.createdAt.toUTCString()
+                },
+                {
+                    title: "Joined At",
+                    text: member.joinedAt.toUTCString()
+                },
+            ],
             thumbnail: user.displayAvatarURL
         }
     }
