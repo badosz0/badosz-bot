2cc03ef171a06a975dfdb8be9726bfb8f6623126
Badosz <badoszk@gmail.com>
2020-04-10 15:06:11 +0200

    user badges

--- src/plugins/info/commands/user.ts
+++ src/plugins/info/commands/user.ts
@@ -1,5 +1,22 @@
-import { Command, Command_output } from "../../../structures/command";
-import { get_user } from "../../../utils/user";
+import { Command, Command_output } from "../../../structures/command"
+import { get_user } from "../../../utils/user"
+const fetch = require('node-fetch')
+
+function is_bit_set(val: number, pos: number) : boolean {
+    return (val & (1 << pos)) != 0
+}
+
+const badges: {[bit: number]: string} = {
+    0: "<:badgeStaff:698151519723323392>",
+    1: "<:badgePartner:698147485486546995>",
+    2: "<:hypeSquad:495281269337227275>",
+    3: "<:badgeBugHunter:698151487091507231>",
+    6: "<:hypeBravery:495281268888698901>",
+    7: "<:hypeBrilliance:495281269157003265>",
+    8: "<:hypeBalance:495281269249146920>",
+    9: "<:badgeSupporter:698151462278135869>",
+    14: "<:badgeBugHunter:698151487091507231>"
+}
 
 export = new Command ({
     trigger : "user",
@@ -9,6 +26,27 @@ export = new Command ({
     {
         const user = await get_user(message, args)
         const member = await message.guild.fetchMember(user)
+        
+        const profile = await fetch(`https://discordapp.com/api/v7/users/${user.id}`, {
+            method: "Get", 
+             headers: { 'Authorization': `Bot ${message.client.token}`}
+        }).then((res: any)=> res.json())
+
+        const flags = profile.public_flags
+
+        let user_badges = ""
+    
+        Object.keys(badges).forEach(badge => {
+            if (is_bit_set(flags, parseInt(badge))) user_badges += `${badges[parseInt(badge)]}`
+        })
+
+        const support = await message.client.guilds.get("340947847728070666")
+        const dt =      await message.client.guilds.get("466179424291651614")
+        const emojis =  await message.client.guilds.get("473959011557965835")
+
+        if (support && support.members.get(user.id)) user_badges += `<:badosz:434830069416984596>`
+        if (dt && dt.members.get(user.id))           user_badges += `<:salio_desire:467000544259735563>`
+        if (emojis && emojis.members.get(user.id))   user_badges += `:smile:`
 
         return {
             fields: [
@@ -70,6 +108,10 @@ export = new Command ({
                 {
                     title: "Joined At",
                     text: member.joinedAt.toUTCString()
+                },
+                {
+                    title: "Badges",
+                    text: user_badges
                 },
             ],
             thumbnail: user.displayAvatarURL
